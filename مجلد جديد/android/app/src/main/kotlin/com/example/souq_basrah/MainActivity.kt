7f5ce0bde626856e27b32d0676c0fda87556ce46
qqqmmmpppzzz2-lang <qqqmmmpppzzz2@gmail.com>
2026-09-09 08:44:56 +0300

    Delete مجلد جديد directory

--- مجلد جديد/android/app/src/main/kotlin/com/example/souq_basrah/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.souq_basrah
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- مجلد جديد/android/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    // أضفنا هذا السطر الخاص بالفايربيس هنا:
-    id("com.google.gms.google-services") version "4.5.0" apply false
-}
